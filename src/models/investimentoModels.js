07c85469f3f6f2b4856f69ba126251e367209dd5
Graciela Rocha <gracielasr694@gmail.com>
2022-07-19 07:36:32 -0300

    feat: foi adicionado as funções findByCod e creatBuy

---
+++ src/models/investimentoModels.js
@@ -0,0 +1,32 @@
+const connection = require('./connection');
+
+const findByCod = async (codAtivo) => {
+  const query = 'SELECT * FROM ativos WHERE codAtivo=?;';
+
+  const [[ativo]] = await connection.execute(query, [codAtivo]);
+
+  return ativo;
+};
+
+const createBuy = async (codCliente, codAtivo, qtdeAtivo) => {
+  const query = 'INSERT INTO operacoes_ativos (codCliente, codAtivo, qtdeAtivo) VALUES(?, ?, ?)';
+
+  const [compraAtivo] = await connection.execute(query, [codCliente, codAtivo, qtdeAtivo]);
+
+  return compraAtivo;
+}
+
+// const getAll = async () => {
+//   const [ativos] = await connection.execute('SELECT * FROM ativos;');
+
+//   return ativos.map(({ codAtivo, quantity, valAtivo }) => ({
+//     codAtivo,
+//     quantity,
+//     valAtivo,
+//   }));
+// };
+
+module.exports = {
+  findByCod, 
+  createBuy,
+}

8df509e825e0f533aab457530344e5f0b3bd4589
Graciela Rocha <gracielasr694@gmail.com>
2022-07-19 09:42:48 -0300

    foi adicionado as rota get busca pelo codCliente e post criando uma venda do cliente

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const investimento = require('./controllers/investimentoController');
 
 router.get('/ativos/:codAtivo', investimento.findByCod);
+router.get('/ativos/:codCliente', investimento.findByClient);
 
-router.post('/investimentos/compras', investimento.createBuy);
+router.post('/investimentos/comprar', investimento.createBuy);
+router.post('/investimentos/comprar', investimento.createSell);
 
 module.exports = router;

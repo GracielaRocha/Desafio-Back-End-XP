173db1d5b8fc712bb74f7cdb7b86f47004d25354
Graciela Rocha <gracielasr694@gmail.com>
2022-07-19 07:29:33 -0300

    feat: alteração do nome da coluna quantity tabela ativo

--- carteiraDigital.sql
+++ carteiraDigital.sql
@@ -6,35 +6,23 @@ USE carteiraDigital;
 
 CREATE TABLE ativos (
     codAtivo INT NOT NULL auto_increment,
-    qtdeAtivo INT NOT NULL,
-    valor DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    valAtivo DECIMAL NOT NULL,
     PRIMARY KEY(codAtivo)
 ) ENGINE=INNODB;
 
-INSERT INTO carteiraDigital.ativos (qtdeAtivo, valor) VALUES
-     (5, 20),
-     (6, 30),
-     (7, 40);
+INSERT INTO carteiraDigital.ativos (quantity, valAtivo) VALUES
+     (5, 100),
+     (6, 300),
+     (7, 400);
 
 CREATE TABLE clientes (
     codCliente INT NOT NULL,
+    saldo DECIMAL NOT NULL,
     PRIMARY KEY(codCliente)
 ) ENGINE=INNODB;
 
-INSERT INTO carteiraDigital.clientes (codCliente) VALUES
-     (1),
-     (2),
-	 (3);
-
-CREATE TABLE operacoes_clientes (
-    cliente_id INT NOT NULL,
-    valor DECIMAL NOT NULL,
-    FOREIGN KEY (cliente_id)
-        REFERENCES clientes (codCliente)
-        ON DELETE CASCADE
-) ENGINE=INNODB;
-
-INSERT INTO carteiraDigital.operacoes_clientes (cliente_id, valor) VALUES
+INSERT INTO carteiraDigital.clientes (codCliente, saldo) VALUES
      (1, 500),
      (2, 600),
 	 (3, 700);
@@ -42,7 +30,7 @@ INSERT INTO carteiraDigital.operacoes_clientes (cliente_id, valor) VALUES
 CREATE TABLE operacoes_ativos (
     cliente_id INT NOT NULL,
     ativo_id INT NOT NULL,
-    quantity INT NOT NULL,
+    qtdeAtivo INT NOT NULL,
     valor DECIMAL NOT NULL,
     CONSTRAINT PRIMARY KEY(cliente_id, ativo_id),
     FOREIGN KEY (cliente_id)
@@ -53,9 +41,9 @@ CREATE TABLE operacoes_ativos (
         ON DELETE CASCADE
 )  ENGINE=INNODB;
 
-SET SQL_SAFE_UPDATES = 0;
+INSERT INTO carteiraDigital.operacoes_ativos (cliente_id, ativo_id, qtdeAtivo, valor) VALUES
+     (1, 2, 30, 20),
+     (2, 1, 20, 25),
+     (3, 3, 40, 30);
 
-INSERT INTO carteiraDigital.operacoes_ativos (cliente_id, ativo_id, quantity, valor) VALUES
-     (1, 2, 100, 20),
-     (2, 1, 200, 25),
-     (3, 3, 400, 30);
+SET SQL_SAFE_UPDATES = 0;

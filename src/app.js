b1e947d343363579959c6dad62d7aae0879f292f
Graciela Rocha <gracielasr694@gmail.com>
2022-07-19 09:41:26 -0300

    feat: foi inserido a rota no arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const router = require('./Routers');
+const router = require('./Routes');
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(router);
+app.use(router);
 
 module.exports = app;

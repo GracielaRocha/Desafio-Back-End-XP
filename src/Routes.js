28c4681d103f1401c5a08ed5fd8102bdd6f170b6
Graciela Rocha <gracielasr694@gmail.com>
2022-07-22 19:11:52 -0300

    feat: foi adicionado a rota put para alterar a quant de ativos vendidos

--- src/Routes.js
+++ src/Routes.js
@@ -8,10 +8,11 @@ const validateValor = require('./middleware/validateValor');
 router.get('/ativos/:codAtivo', investimento.findByCod);
 router.get('/clientes/:codCliente', investimento.findByClient);
 router.get('/conta/:codCliente', investimento.getBalance);
-// router.post('/investimentos/comprar', investimento.createBuy);
+router.post('/investimentos/comprar', investimento.createBuy);
 // router.post('/investimentos/vender', investimento.createSell);
 
 router.put('/conta/saque/:codCliente', validateValor, investimento.updateSaque);
 router.put('/conta/deposito/:codCliente', validateValor, investimento.updateDeposito);
+router.put('/conta/ativo/:codAtivo', investimento.updateAssest);
 
 module.exports = router;
